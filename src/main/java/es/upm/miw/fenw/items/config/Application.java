20839b97ee368084794249ccde7ad4db5d41585c
jbernal <j.bernal@upm.es>
2017-12-09 19:53:50 +0100

    Swagger 2

--- src/main/java/es/upm/miw/fenw/items/config/Application.java
+++ src/main/java/es/upm/miw/fenw/items/config/Application.java
@@ -3,6 +3,7 @@ package es.upm.miw.fenw.items.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,6 +12,7 @@ import es.upm.miw.fenw.items.daos.DaoFactory;
 import es.upm.miw.fenw.items.daos.memory.DaoMemoryFactory;
 
 @SpringBootApplication(scanBasePackages = {Names.ASPECT,Names.RESOURCES, Names.DTOS, Names.CONTROLLERS, Names.DAOS})
+@Import(SwaggerConfig.class)
 public class Application {
     
     @Bean
